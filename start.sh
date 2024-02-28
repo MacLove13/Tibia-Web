7cf0e115487ebd572cbe983a8de35aa706bb77d5
Felipe <felipe.barbosa@iugu.com>
2024-02-28 14:15:25 -0300

    hotfix/Start

--- start.sh
+++ start.sh
@@ -11,10 +11,16 @@ nvm use 16
 ruby -v
 node -v
 
-sudo kill -9 `sudo lsof -t -i:3030`
+PORT=3030
+PID=$(lsof -ti:$PORT)
+
+if [ ! -z "$PID" ]; then
+  echo "Killing process on port $PORT"
+  sudo kill -9 $PID
+fi
 
 sudo chown -R $USER tmp
 sudo chown -R $USER public
 
 echo "Run Rails Server"
-bundle exec puma -C /tibia/web/config/puma.rb
+bundle exec puma -C /root/tibia/web/config/puma.rb
